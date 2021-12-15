0f37a5391df7ac75b585c8ce8b42e697c78af293
Vedant Lohbare <vedant.lohbare@gmail.com>
2021-12-15 16:16:10 +0530

    SEO

--- components/SEO/index.js
+++ components/SEO/index.js
@@ -3,28 +3,27 @@ import React from "react";
 
 export function SEO({ title, description }) {
   if (title === undefined) {
-    title = "Song-tastics";
+    title = "Songtastic";
   } else {
-    title = title + " | Better Product Manager";
+    title = title + " | Songtastic";
   }
   if (description === undefined) {
-    description =
-      "Tests and quizes, designed to give you a little taste of what the PM role is like.";
+    description = "Fun quiz to test your song knowlege!";
   }
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
-      <meta property="og:url" content="www.betterproductmanager.com" />
+      <meta property="og:url" content="https://quiz.songtastic.xyz/" />
       <meta property="og:type" content="website" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:creator" content="Vedant Lohbare" />
+      <meta name="twitter:creator" content="Anishi Raj" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta
         property="og:image"
-        content="https://betterproductmanager.s3.ap-south-1.amazonaws.com/Screen1.png"
+        content="https://quiz.songtastic.xyz/meta.png"
       />
     </Head>
   );
